cf85988c7d4ef220c9c99cf4997b1d9eac535fb2
Jonathan Riley <Jonrrile@ut.utm.edu>
2020-10-26 11:51:58 -0500

    Added Bet table in SQL and set up the Repository, Model, and Controller.

---
+++ Capstone/Controllers/BetController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Capstone.Models;
+using Capstone.Repositories;
+
+namespace Tabloid.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BetController : ControllerBase
+    {
+        private readonly IBetRepository _betRepository;
+
+        public BetController(IBetRepository betRepository)
+        {
+            _betRepository = betRepository;
+        }
+
+
+        [HttpGet("GetAllBetsByTeam/{id}")]
+        public IActionResult GetAllBetsByTeam(int id)
+        {
+            return Ok(_betRepository.GetAllBetsByTeamId(id));
+        }
+    }
+}
